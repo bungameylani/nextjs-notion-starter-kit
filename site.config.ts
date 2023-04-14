dbc88aa2d49b32ce37ebfbf55ad4144f9a3417bd
bungameylani <120106589+bungameylani@users.noreply.github.com>
2023-04-14 20:23:21 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Bunga Meylani',
-  domain: 'vercel.bungameylani.eu.org',
+  domain: 'bungameylani.eu.org',
   author: 'Bunga Meylani',
 
   // open graph metadata (optional)
